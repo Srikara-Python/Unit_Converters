8cbf4ccc0b780089b941dc19b9d3fe03972ac1da
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-21 11:53:54 +0530

    successfully add kilo to meter

--- main.py
+++ main.py
@@ -6,8 +6,6 @@
 
 from tkinter import * 
 
-
-
 root = Tk()
 root.title("All in One Converter - Srikara Narasimha")
 root.geometry("500x500+0+0")
@@ -40,10 +38,42 @@ def met_kilometer_answer():
 
     meter_kilo.mainloop()
 
+
+
+def kilo_meter():
+    global kilo_meter
+    global kilo_meter_entry
+    kilo_meter = Toplevel()
+    kilo_meter.title("Meter To Kilometer")
+    kilo_meter_label1 = Label(kilo_meter, text="Please enter Kilometer value below:- ")
+    kilo_meter_label1.grid(row=0, column=0)
+    kilo_meter_entry = Entry(kilo_meter)
+    kilo_meter_entry.grid(row=1, column=0)
+    kilo_meter_enterbutton = Button(kilo_meter, text="Enter", command=kilo_meter_answer)
+    kilo_meter_enterbutton.grid(row=1, column=1)  
+
+def kilo_meter_answer():
+    global kilo_meter_label2
+    check_label2 = "kilo_meter_label2" in globals()
+    if check_label2 == True:
+        kilo_meter_label2.destroy()
+    else:
+        pass
+    user_input_kilo_meter = float(kilo_meter_entry.get())
+    answer_kilo_meter = user_input_kilo_meter * 1000
+    kilo_meter_label2 = Label(kilo_meter, text=str(answer_kilo_meter) + (" m"))
+    kilo_meter_label2.grid(row=2, column=0)
+
+    kilo_meter.mainloop()
+
+
+
 def main_window():
     start_button.destroy()
     met_kil_button = Button(root, text="Meter To Kilometer", command=met_kilometer)
     met_kil_button.grid(row=1,column=0)
+    kil_met_button = Button(root, text="Kilometer to Meter", command=kilo_meter)
+    kil_met_button.grid(row=1,column=1)
     
 start_button = Button(root, text="Click ME!", command=main_window)
 start_button.pack()
