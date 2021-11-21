bbb312ea6fa3c7c784b53fb791888859d4ffb481
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-21 10:45:23 +0530

    add button and window

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 # This is the main file for this app
 # Start Date - 20.11.21
 # Srikara Narasimha
+
+from tkinter import * 
+
+
+
+root = Tk()
+root.title("All in One Converter - Srikara Narasimha")
+root.geometry("500x500+0+0")
+root.eval('tk::PlaceWindow . center')
+orig_color = root.cget("background")
+
+
+def main_window():
+    start_button.destroy()
+    
+start_button = Button(root, text="Click ME!", command=main_window)
+start_button.pack()
+
+
+
+
+
+
+
+root.mainloop()
