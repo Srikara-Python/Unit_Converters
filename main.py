2fa27933565e0eb23921470200314c09c028d959
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-21 11:01:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,18 @@ root.geometry("500x500+0+0")
 root.eval('tk::PlaceWindow . center')
 orig_color = root.cget("background")
 
+def met_kilometer():
+    meter_kilo = Toplevel()
+    meter_kilo.title("Meter To Kilometer")
+    meter_kilo_entry = Entry(meter_kilo)
+    meter_kilo_entry.grid(row=0, column=0)
+
+    meter_kilo.mainloop()
 
 def main_window():
     start_button.destroy()
+    met_kil_button = Button(root, text="Meter To Kilometer", command=met_kilometer)
+    met_kil_button.grid(row=1,column=0)
     
 start_button = Button(root, text="Click ME!", command=main_window)
 start_button.pack()
