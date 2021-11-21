d66debf29d517ca6f8767b5ef02ba55665d61342
Srikara Narasimha <srikara07.sn@hmail.com>
2021-11-21 11:23:45 +0530

    successfully ad meter to kilo

--- main.py
+++ main.py
@@ -15,10 +15,28 @@ root.eval('tk::PlaceWindow . center')
 orig_color = root.cget("background")
 
 def met_kilometer():
+    global meter_kilo
+    global meter_kilo_entry
     meter_kilo = Toplevel()
     meter_kilo.title("Meter To Kilometer")
+    meter_kilo_label1 = Label(meter_kilo, text="Please enter meter value below:- ")
+    meter_kilo_label1.grid(row=0, column=0)
     meter_kilo_entry = Entry(meter_kilo)
-    meter_kilo_entry.grid(row=0, column=0)
+    meter_kilo_entry.grid(row=1, column=0)
+    meter_kilo_enterbutton = Button(meter_kilo, text="Enter", command=met_kilometer_answer)
+    meter_kilo_enterbutton.grid(row=1, column=1)
+
+def met_kilometer_answer():
+    global meter_kilo_label2
+    check_label2 = "meter_kilo_label2" in globals()
+    if check_label2 == True:
+        meter_kilo_label2.destroy()
+    else:
+        pass
+    user_input_met_kilo = float(meter_kilo_entry.get())
+    answer_met_kilo = user_input_met_kilo / 1000
+    meter_kilo_label2 = Label(meter_kilo, text=str(answer_met_kilo) + (" Km"))
+    meter_kilo_label2.grid(row=2, column=0)
 
     meter_kilo.mainloop()
 
